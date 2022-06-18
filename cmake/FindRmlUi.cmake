6cfcc99d5a94accc733f7535b28ee4769fa6ebae
Vincent Alleaume <22142836+skons35@users.noreply.github.com>
2022-06-18 19:19:51 +0200

    - first commit targeting MorphOS port at the end - here review the RmlUi build & use

--- cmake/FindRmlUi.cmake
+++ cmake/FindRmlUi.cmake
@@ -3,47 +3,49 @@ FIND_PATH(RMLUI_HEADERS RmlUi/Core.h  # find RmlUi/Core.h and set it in RMLUI_HE
   PATH_SUFFIXES Include  # postfix the path 'Include' when searching the paths below for 'RmlUi/Core.h'
   PATHS
   $ENV{RMLUI}  # Use environment variable RMLUI and see if $RMLUI/include/RmlUi/Core.h exists
-  ${RMLUI_DIR}/RmlUi
-  ~/Library/Frameworks
-  /Library/Frameworks
-  /usr/local
-  /usr
-  /sw # Fink
-  /opt/local # DarwinPorts
-  /opt/csw # Blastwave
-  /opt
+  #${RMLUI_DIR}/RmlUi
+  #~/Library/Frameworks
+  #/Library/Frameworks
+  #/usr/local
+  #/usr
+  #/sw # Fink
+  #/opt/local # DarwinPorts
+  #/opt/csw # Blastwave
+  #/opt
 )
 
 FIND_LIBRARY(RMLUI_CORE RmlCore
   HINTS
-  PATH_SUFFIXES lib lib64 libs/Win32 libs/Win64
+  #PATH_SUFFIXES lib lib64 libs/Win32 libs/Win64
+  PATH_SUFFIXES build
   PATHS
   $ENV{RMLUI}
-  ${RMLUI_STATIC_DIR}
-  ~/Library/Frameworks
-  /Library/Frameworks
-  /usr/local
-  /usr
-  /sw
-  /opt/local
-  /opt/csw
-  /opt
+  #${RMLUI_STATIC_DIR}
+  #~/Library/Frameworks
+  #/Library/Frameworks
+  #/usr/local
+  #/usr
+  #/sw
+  #/opt/local
+  #/opt/csw
+  #/opt
 )
 
 FIND_LIBRARY(RMLUI_DEBUGGER RmlDebugger
   HINTS
-  PATH_SUFFIXES lib lib64 libs/Win32 libs/Win64
+  #PATH_SUFFIXES lib lib64 libs/Win32 libs/Win64
+  PATH_SUFFIXES build
   PATHS
   $ENV{RMLUI}
-  ${RMLUI_STATIC_DIR}
-  ~/Library/Frameworks
-  /Library/Frameworks
-  /usr/local
-  /usr
-  /sw
-  /opt/local
-  /opt/csw
-  /opt
+  #${RMLUI_STATIC_DIR}
+  #~/Library/Frameworks
+  #/Library/Frameworks
+  #/usr/local
+  #/usr
+  #/sw
+  #/opt/local
+  #/opt/csw
+  #/opt
 )
 
 SET(RMLUI_FOUND "NO")
